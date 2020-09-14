f1b83f23c554c9751fb370df693c876c03d8fc8b
Alfer <71255030+AlferGS@users.noreply.github.com>
2020-09-14 11:57:03 +0300

    Update lab1.cpp

--- lab1/lab1/lab1.cpp
+++ lab1/lab1/lab1.cpp
@@ -2,19 +2,23 @@
 #include <string>
 #include <iomanip>
 
+/*Ввод строки и вывод её в обратном порядке. Ширина
+поля в 1,5 раза превышает длинну строки. Результат
+вывести в верхнем регистре*/
+
 std::string reverse(std::string str, int size);
 
 int main()
 {
 	std::string str;
-	int size = 0;
-	std::cout << "Enter string\n";
+	int size = 0;				
+	std::cout << "Enter string\n";				//Ввод строки, определение её длинны и отправка в функцию reverse
 	std::getline(std::cin, str);
 	size = str.size();
 	str = reverse(str, size);
-	std::cout << std::setw(size*1.5) << str << "\n";
+	std::cout << std::setw(size*1.5) << str << "\n";	//Вывод результата с установкой ширины поля
 }
-
+//Функция реверса строки с переводом в верхний регистр
 std::string reverse(std::string str, int size)
 {
 	char temp;
