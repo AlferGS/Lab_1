a161924dd7ae253f64624bdc580834cec9491eff
Alfer <71255030+AlferGS@users.noreply.github.com>
2020-09-14 11:47:06 +0300

    Add files via upload

---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <iomanip>
+
+std::string reverse(std::string str, int size);
+
+int main()
+{
+	std::string str;
+	int size = 0;
+	std::cout << "Enter string\n";
+	std::getline(std::cin, str);
+	size = str.size();
+	str = reverse(str, size);
+	//str = toupper(str);
+	std::cout << std::setw(size*1.5) << str << "\n";
+}
+
+std::string reverse(std::string str, int size)
+{
+	char temp;
+	int i = 0;
+	size--;
+	while( i < size )
+	{
+		str[i] = toupper(str[i]);
+		str[size] = toupper(str[size]);
+		temp = str[i];
+		str[i++] = str[size];
+		str[size--] = temp;
+	}
+	return str;
+}
